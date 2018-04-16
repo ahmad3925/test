718642db15f08182577a4bc9a8e5d4a5e4452d12
Ahmad Hussain <ah3925@gmail.com>
2018-04-16 15:40:38 +0530

    new

--- abc.java
+++ abc.java
@@ -1,12 +1,18 @@
 
 class Hello {
 
-	public static void main(String[] args) 
-	{
+	
+public static void main(String[] args) 
+
+{
 	 
 
+
 	 System.out.println("hello");
+System.out.println("hello world");
 	 
+
 	}
+
 
 }
